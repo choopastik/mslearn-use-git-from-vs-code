a6c3c79106acb3db7152b9f5b0162e96fb03b093
choopastik <choopastik@gmail.com>
2021-04-21 21:39:27 +0300

    test

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Git!
+  res.render('index', { title: 'Hello Git!'})
+})
